d87ec7667d7a65bd49f68cff21566626fb144828
Shami Tomita <shamitomita@gmail.com>
2021-01-24 19:07:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
   
   def self.genre_count 
     genre_count = Hash.new(0)
-    @@genres.each {|genre| genre_count
+    @@genres.each {|genre| genre_count[genre] +=1
   end 
   
   def self.artist_count
